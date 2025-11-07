9b7596e89e1d9d8b03f1e47e3e1ceddba51dddbd
Naol Selemon <naol.selemon.scitech@gmail.com>
2025-11-07 12:14:16 +0300

    core array implementation

---
+++ Arrays/README.md
@@ -0,0 +1,9 @@
+### Four(4)  fundamental operations of Arrays:
+ - lookup: O(1)
+ - Push or Pop : O(1)
+ - Insert: O(n)
+ - Delete: O(n)
+
+### Static and Dynamic Arrays
+ - Static: Array of fixed size and size do not change over time.
+ - Dynamic: Array of variable size and memory is handled by the program it self.
---
+++ Arrays/coreArrayImplementation.py
@@ -0,0 +1,50 @@
+class fundamentalArray:
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+    def get(self, index):
+        return self.data[index]
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+        return self.length
+    def pop(self):
+        if self.length == 0:
+            return None
+        lastItem = self.data[self.length - 1]    
+        del self.data[self.length -1]
+        self.length -= 1
+        return lastItem
+    
+    def delete(self, index:int):
+        item = self.data[index]
+        self.shiftItems(index)
+        return item
+
+    def shiftItems(self, index: int):
+        if (index == self.length -1):
+            del self.data[index]
+            self.length -= 1
+
+        for item in range(index, self.length):
+            if (item == self.length -1):
+                del self.data[item]
+                return
+            self.data[item] = self.data[item+1]
+            
+
+
+
+
+    
+newArray = fundamentalArray()
+newArray.push("hi")
+newArray.push("you")
+newArray.push("are")
+newArray.push("nice")
+
+print(newArray.data)
+# newArray.pop()
+#print(newArray.data)
+newArray.delete(1)
+print(newArray.data)
