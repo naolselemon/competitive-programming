5e8c21e9c42d51503ab8179e2a83709d31ff465e
Naol Selemon <naol.selemon.scitech@gmail.com>
2026-06-07 18:00:05 +0300

    refactor: concatenate singylinkedlist operations

---
+++ LinkedList/SinglyLinkedList/operations.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def append(self, data):
+
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            self.length += 1
+            return
+
+        last_node = self.head
+
+        while last_node.next:
+            last_node = last_node.next
+
+        last_node.next = new_node
+        self.length += 1
+
+    def prepend(self, data):
+
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert(self, index, data):
+        newNode = Node(data)
+        current = self.head
+        counter = 0
+        if index >= self.length:
+            return self.append(data)
+        while counter != index - 1:
+            current = current.next
+            counter += 1
+
+        leader = current
+        holdingPointer = leader.next
+        leader.next = newNode
+        newNode.next = holdingPointer
+
+    def remove(self, index):
+        current = self.head
+        counter = 0
+        if index >= self.length:
+            print("Index out of bound!")
+        if index < 0:
+            print("Index must be a positive integer!")
+        while counter != index - 1:
+            current = current.next
+            counter += 1
+        leader = current
+        unWantedNode = leader.next
+        leader.next = unWantedNode.next
+
+    def display(self):
+        current = self.head
+        elements = []
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        print("->".join(elements))
+
+
+myHead = LinkedList()
+myHead.append(1)
+myHead.append(2)
+myHead.insert(1, 100)
+myHead.insert(10, 200)
+myHead.remove(1)
+myHead.remove(-10)
+myHead.display()
