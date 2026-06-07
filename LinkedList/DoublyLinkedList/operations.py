4e52b06fde42a8c88a44abb7f083f2422810da90
Naol Selemon <naol.selemon.scitech@gmail.com>
2026-06-07 18:11:42 +0300

    add: display and append implementation
    append: add element at the end of the list
    display: print element added to the list

--- LinkedList/DoublyLinkedList/operations.py
+++ LinkedList/DoublyLinkedList/operations.py
@@ -1,1 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" <-> ")
+            current = current.next
+        print("None")
+
+    def append(self, data):
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+            self.length += 1
+            return
+
+        lastNode = self.head
+        while lastNode.next:
+            lastNode = lastNode.next
+
+        lastNode.next = newNode
+        newNode.prev = lastNode
+        self.length += 1
 
