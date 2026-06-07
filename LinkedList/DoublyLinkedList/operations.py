82918f318a08029010d4daf78eeec0e44529f8b9
Naol Selemon <naol.selemon.scitech@gmail.com>
2026-06-07 18:14:27 +0300

    add: insert and prepend operation implementation
    insert: add element at specific position
    prepend: add at the start of the list

--- LinkedList/DoublyLinkedList/operations.py
+++ LinkedList/DoublyLinkedList/operations.py
@@ -33,3 +33,36 @@ class DoublyLinkedList:
         newNode.prev = lastNode
         self.length += 1
 
+    def prepend(self, data):
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+            self.length += 1
+            return
+
+        newNode.next = self.head
+        self.head.prev = newNode
+        self.head = newNode
+        self.length += 1
+
+    def insert(index, data):
+        newNode = Node(data)
+        counter = 0
+        current = self.head
+
+        if not current:
+            self.head = newNode
+            return
+
+        while counter != index - 1:
+            current = current.next
+            counter += 1
+
+        leader = current
+        followingNode = leader.next
+        leader.next = newNode
+        newNode.prev = leader
+        newNode.next = followingNode
+        followingNode.prev = newNode
+
