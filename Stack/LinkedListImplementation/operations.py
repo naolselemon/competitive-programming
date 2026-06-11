96229940be3123a85e91a80e7bb85c5b5b56f030
Naol Selemon <naol.selemon.scitech@gmail.com>
2026-06-11 16:03:52 +0300

    linked list implementation of stack

--- Stack/LinkedListImplementation/operations.py
+++ Stack/LinkedListImplementation/operations.py
@@ -3,17 +3,20 @@ class Node:
         self.data = data
         self.next = None
 
+
 class Stack:
     def __init__(self):
         self.top = None
         self.bottom = None
         self.length = 0
+
     def peek(self):
         if self.length == 0:
             print("Stack is empty!")
             return
 
         print(self.top.data)
+
     def push(self, data):
         new_node = Node(data)
 
@@ -28,3 +31,31 @@ class Stack:
 
         return self
 
+    def pop(self):
+
+        if self.length == 0:
+            print("There is no element to pop!")
+            return None
+
+        holdingPointer = self.top
+        self.top = self.top.next
+        self.length -= 1
+        return holdingPointer
+
+    def display(self):
+        current = self.top
+        elements = []
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        print(" | ".join(elements))
+
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.peek()
+    stack.display()
+    stack.pop()
+    stack.display()
